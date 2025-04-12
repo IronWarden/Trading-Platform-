c7d167a9c050f1ae2dab04d532618e213e242195
IronWarden <example.org>
2025-04-12 16:47:29 -0400

    minor changes

--- data.py
+++ data.py
@@ -2,13 +2,20 @@ import yfinance as yf
 import pandas as pd
 import redis 
 import time 
+import sqlite3
 
 r = redis.Redis(host='localhost', port=6379, db=0)
 
-
+def fetch_stock():
+    
+    return stock
 
 if __name__ == "__main__":
+    stock_tickers = []
+    with sqlite3.connect('/instance/flaskr.sqlite') as conn:
+        db = conn.cursor()
+        stocks = db.execute("SELECT Symbol FROM Stocks")
+        stock_tickers = [row[0] for row in stocks.fetchall()]
+
     while True:
-        tickers = r.get('tickers').decode('utf-8')
-        tickers = tickers.split(',')
-        for ticker in tickers:
+
