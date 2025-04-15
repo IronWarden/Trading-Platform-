c358916f81ec53512f67ecae4c63331bb19023bc
IronWarden <example.org>
2025-04-15 17:01:49 -0400

    tried using yfinance download instead of history

--- data.py
+++ data.py
@@ -1,21 +1,23 @@
 import yfinance as yf
 import pandas as pd
-import redis 
-import time 
+import redis
+import time
 import sqlite3
 
-r = redis.Redis(host='localhost', port=6379, db=0)
+r = redis.Redis(host="localhost", port=6379, db=0)
 
-def fetch_stock():
-    
-    return stock
+
+def fetch_stock(tickers):
+    data = yf.download(tickers, period="1mo")
+    return data
+
 
 if __name__ == "__main__":
     stock_tickers = []
-    with sqlite3.connect('/instance/flaskr.sqlite') as conn:
+    with sqlite3.connect("./instance/flaskr.sqlite") as conn:
         db = conn.cursor()
         stocks = db.execute("SELECT Symbol FROM Stocks")
         stock_tickers = [row[0] for row in stocks.fetchall()]
 
-    while True:
-
+    df = fetch_stock(stock_tickers)
+    df.to_csv("data.csv")
